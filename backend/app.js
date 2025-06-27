27fb94715575681ae28383cdadc1688629bc632d
vjy-07 <gvijaycrl@gmail.com>
2025-06-27 15:10:39 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
+
+const allowedOrigins = [
+  "http://localhost:3000", // local dev
+  "https://reicperevive-frontend.onrender.com/", // replace with your actual frontend URL
+];
 
 // Connect to MongoDB
 connectDB();
 
 // Middleware
-app.use(express.json());  // This line parses incoming JSON requests
-app.use(cors());
+app.use(express.json()); // This line parses incoming JSON requests
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 // Define Routes
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
 
 const PORT = process.env.PORT || 5001;
 
